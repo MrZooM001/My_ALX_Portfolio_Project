d6e863bd236fcc2335e8cc8fc8f14f0f8666596f
Hazem Magdy <hhazem0010@gmail.com>
2024-03-15 15:29:03 +0200

    update env vars

--- config.py
+++ config.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 from decouple import config
-from os import path
+from os import path, getenv
 
 
 BASE_DIR = path.join(path.dirname(path.realpath(__file__)))
 
 
 class BaseConfig:
-    SECRET_KEY = config("SECRET_KEY")
-    SQLALCHEMY_TRACK_MODIFICATIONS = config("SQLALCHEMY_TRACK_MODIFICATIONS", cast=bool)
+    SECRET_KEY = getenv('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = getenv("SQLALCHEMY_TRACK_MODIFICATIONS")
 
 
 class DevConfig(BaseConfig):
-    SQLALCHEMY_DATABASE_URI = config("SQLALCHEMY_DATABASE_URI")
-    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = getenv("JAWSDB_URL")
+    DEBUG = getenv("DEBUG")
     SQLALCHEMY_ECHO = True
-    TEMPLATES_AUTO_RELOAD = config("TEMPLATES_AUTO_RELOAD")
+    TEMPLATES_AUTO_RELOAD = getenv("TEMPLATES_AUTO_RELOAD")
     SESSION_COOKIE_HTTPONLY = False
     # TEMPLATES_FOLDER = config("TEMPLATES_FOLDER")
 
